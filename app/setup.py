d04d47f43849d0b2138fb8bd965172864c28da88
Gary Lam <me@slashiee.com>
2025-01-13 17:47:29 +0800

    Added setup.py to make bazi an installable package

---
+++ app/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bazi",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package needs here
+    ],
+    description="A package for Bazi calculations",
+    author="Your Name",
+    author_email="garylamj@gmail.com",
+    url="https://github.com/gazaay/four-pillars",
+)
