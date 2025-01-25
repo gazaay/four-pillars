d9d9cf961a927c3dbb5eaff54e1d9baf2b05f409
Gary Lam <me@slashiee.com>
2025-01-25 12:37:55 +0800

    fixing build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bazi",
-    version="0.1",
+    version="1.0.0",
     # packages=find_packages(),
     packages=["app"],  # Explicitly include the 'app' package
     package_dir={"app": "app"},  # Map the 'app' package to the 'app' directory
