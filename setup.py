ab589326724fe0140df8ddca10d23209ccbf7e73
Gary Lam <me@slashiee.com>
2025-02-03 11:05:32 +0800

    update daiyun

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bazi",
-    version="1.2.2",
+    version="1.2.3",
     # packages=find_packages(),
     packages=["app"],  # Explicitly include the 'app' package
     package_dir={"app": "app"},  # Map the 'app' package to the 'app' directory
